d4e51b345c8ae9cdd73b7dfeb2abb24b0f1b4007
My Ancestors' King <chosenbrain@Mys-MacBook-Pro.local>
2026-08-09 18:53:35 -0700

    Fix chat privacy QA harness FlutterError import

--- vault_ai_frontend/integration_test/qa_chat_privacy_acceptance_test.dart
+++ vault_ai_frontend/integration_test/qa_chat_privacy_acceptance_test.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter/foundation.dart';
 import 'package:integration_test/integration_test.dart';
 import 'package:vault_ai_frontend/main.dart' as app;
 
